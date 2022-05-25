7d7ceebab2c40c33c7dfe5e6d4bc41933cde14ab
Mike <61545789+chiefbaki@users.noreply.github.com>
2022-05-25 11:57:36 +0300

    Update Bot.py

--- Bot/Bot/Bot.py
+++ Bot/Bot/Bot.py
@@ -28,7 +28,7 @@ class VkBot:
         """
         Конструктор
         """
-        self.vk_session = vk_api.VkApi(token='6b36926d25a772b75c0d608cd7c272521630175dabe9518bc55bfc1a1487aa988537e980df5e27bbc0be3')
+        self.vk_session = vk_api.VkApi(token='9dfbe5b54005cdb0be6fbd84133db0e023a690cab09d7fb13241195a6b537379c2e0b17ae8d09edf331ee')
         self.vk = self.vk_session.get_api()
         self.longpoll = VkLongPoll(self.vk_session)
         self.users_to_set_group: set = set()
@@ -255,7 +255,7 @@ class VkBot:
         Возвращает расписание преподавателя на указанную неделю
         """
         now = date.isocalendar()
-        week = now.week - 5
+        week = now.week + scfg.WEEK_DELTA
         week_even = (week + 1) % 2  # Является ли неделя чётной
         out = []
         tmp = []
@@ -305,6 +305,12 @@ class VkBot:
         if week_index == 6:
             return [[] * 4] * 6
         return week[week_index]
+
+
+
+
+
+
 
     def _get_corona_stat(self, extra_url: str = '') -> tuple[str, list[Any], list[Any]]:
         """
@@ -453,83 +459,45 @@ class VkBot:
         self._send_message_with_attachments(user_id=user_id, text='Сейчас', attachments=attachments)
 
 
-    def _show_5days_weather(self, user_id: int) -> None:
-        upload = VkUpload(self.vk_session)
-        attachments = []
-        image = Image.open('weather_pattern.jpg')
-        # img = image.resize((400, 500))
-        img = image.resize((400, 260))
-        img = img.convert('RGB')
-        idraw = ImageDraw.Draw(img)
-        title = ImageFont.truetype(font='lato.ttf', size=30)
-        font = ImageFont.truetype(font='lato.ttf', size=18)
-        # title2 = ImageFont.truetype(size=30)
-        idraw.text((10, 10), 'Погода в Москве', font=title, fill="white")
-        response = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                                params={'q': 'moscow,ru', 'units': 'metric',
-                                        'APPID': '3051c8da538a2e104685783dae4796f4', 'lang': 'ru'})
-        info = response.json()
-        for i in range (0, 5):
-            img_name = info['list'][2]['weather']['icon']
-            im = Image.open(urlopen('https://openweathermap.org/img/wn/{}@4x.png'.format(img_name)))
-            img.paste(im, (95, 20), im.convert('RGBA'))
-            status = info['weather'][i]['description'].capitalize()
-            temp = str(int(info['main']['temp_min'])) + ' - ' + str(int(info['main']['temp_max']))
-            pressure = str(int(float(info['main']['pressure']) / 1.33))
-            humidity = str(info['main']['humidity'])
-            wind_speed = info['wind']['speed']
-            wind_slug = self._get_wind_slug(float(wind_speed)).lower()
-            wind_deg_slug = self._get_wind_deg_slug(info['wind']['speed'])
-            weather = '{}, температура: {}°C\nДавление: {} мм рт. сб., влажность: {}%\nВетер: {}, {} м/с, {}'.format(status,
-                    temp,pressure,humidity,wind_slug,wind_speed,wind_deg_slug)
-            idraw.text((10, 185), weather, font=font, fill="white")
-            img.save('data/weather_card.png')
+    # def _show_5days_weather(self, user_id: int) -> None:
+    #     upload = VkUpload(self.vk_session)
+    #     attachments = []
+    #     image = Image.open('weather_pattern.jpg')
+    #     # img = image.resize((400, 500))
+    #     img = image.resize((400, 260))
+    #     img = img.convert('RGB')
+    #     idraw = ImageDraw.Draw(img)
+    #     title = ImageFont.truetype(font='lato.ttf', size=30)
+    #     font = ImageFont.truetype(font='lato.ttf', size=18)
+    #     # title2 = ImageFont.truetype(size=30)
+    #     idraw.text((10, 10), 'Погода в Москве', font=title, fill="white")
+    #     response = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    #                             params={'q': 'moscow,ru', 'units': 'metric',
+    #                                     'APPID': '3051c8da538a2e104685783dae4796f4', 'lang': 'ru'})
+    #     info = response.json()
+    #
+    #         img_name = info['list'][2]['weather']['icon']
+    #         im = Image.open(urlopen('https://openweathermap.org/img/wn/{}@4x.png'.format(img_name)))
+    #         img.paste(im, (95, 20), im.convert('RGBA'))
+    #         status = info['weather'][i]['description'].capitalize()
+    #         temp = str(int(info['main']['temp_min'])) + ' - ' + str(int(info['main']['temp_max']))
+    #         pressure = str(int(float(info['main']['pressure']) / 1.33))
+    #         humidity = str(info['main']['humidity'])
+    #         wind_speed = info['wind']['speed']
+    #         wind_slug = self._get_wind_slug(float(wind_speed)).lower()
+    #         wind_deg_slug = self._get_wind_deg_slug(info['wind']['speed'])
+    #         weather = '{}, температура: {}°C\nДавление: {} мм рт. сб., влажность: {}%\nВетер: {}, {} м/с, {}'.format(status,
+    #                 temp,pressure,humidity,wind_slug,wind_speed,wind_deg_slug)
+    #         idraw.text((10, 185), weather, font=font, fill="white")
+    #         img.save('data/weather_card.png')
+    #
+    #         photo = upload.photo_messages('data/weather_card.png')[i]
+    #         attachments.append("photo{}_{}".format(photo["owner_id"], photo["id"]))
+    #
+    #     self._send_message_with_attachments(user_id=user_id, text='на 5 дней', attachments=attachments)
 
-            photo = upload.photo_messages('data/weather_card.png')[i]
-            attachments.append("photo{}_{}".format(photo["owner_id"], photo["id"]))
+    #def _show_tomorrow_weather(self, user_id: int) -> None:
 
-        self._send_message_with_attachments(user_id=user_id, text='на 5 дней', attachments=attachments)
-
-    def _show_tomorrow_weather(self, user_id: int) -> None:
-        upload = VkUpload(self.vk_session)
-        attachments = []
-        image = Image.open('weather_pattern.jpg')
-        # img = image.resize((400, 500))
-        img = image.resize((400, 260))
-        img = img.convert('RGB')
-        idraw = ImageDraw.Draw(img)
-        title = ImageFont.truetype(font='lato.ttf', size=30)
-        font = ImageFont.truetype(font='lato.ttf', size=18)
-        # title2 = ImageFont.truetype(size=30)
-        idraw.text((10, 10), 'Погода в Москве', font=title, fill="white")
-        response = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                                params={'q': 'moscow,ru', 'units': 'metric',
-                                        'APPID': '3051c8da538a2e104685783dae4796f4', 'lang': 'ru'})
-        info = response.json()
-        img_name = info['weather'][0]['icon']
-        im = Image.open(urlopen('https://openweathermap.org/img/wn/{}@4x.png'.format(img_name)))
-        img.paste(im, (95, 20), im.convert('RGBA'))
-        status = info['weather'][0]['description'].capitalize()
-        temp = str(int(info['main']['temp_min'])) + ' - ' + str(int(info['main']['temp_max']))
-        pressure = str(int(float(info['main']['pressure']) / 1.33))
-        humidity = str(info['main']['humidity'])
-        wind_speed = info['wind']['speed']
-        wind_slug = self._get_wind_slug(float(wind_speed)).lower()
-        wind_deg_slug = self._get_wind_deg_slug(info['wind']['speed'])
-        weather = '{}, температура: {}°C\nДавление: {} мм рт. сб., влажность: {}%\nВетер: {}, {} м/с, {}'.format(status,
-                                                                                                                 temp,
-                                                                                                                 pressure,
-                                                                                                                 humidity,
-                                                                                                                 wind_slug,
-                                                                                                                 wind_speed,
-                                                                                                                 wind_deg_slug)
-        idraw.text((10, 185), weather, font=font, fill="white")
-        img.save('data/weather_card.png')
-
-        photo = upload.photo_messages('data/weather_card.png')[0]
-        attachments.append("photo{}_{}".format(photo["owner_id"], photo["id"]))
-
-        self._send_message_with_attachments(user_id=user_id, text='Сейчас', attachments=attachments)
 
     #def _show_today_weather(self, user_id: int) -> None:
 
